4d13dba917513f185ec87b8c3f79572930e91bef
mejri mahdi <mejri.mahdi@gmail.com>
2024-10-21 20:51:50 +0100

    connexion_db

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 //require express !
 const  express = require ('express');
+const connect = require("./config/config")
+
+//create instance
+const app=express()
+
+//middleWare
+app.use(express.json())
+
+//create PORT
+const PORT=8000
+
+//create server
+app.listen(PORT,(error)=>{
+    error?console.log(error):console.log(`the server is running on port ${PORT}`)
+})
+
+connect()
