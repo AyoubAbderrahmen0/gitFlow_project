dd90eed515ac82a17259f99a7dbe86d300d8ef51
Malek9707 <cherifmalek48@gmail.com>
2024-10-21 20:37:57 +0100

    Création_Servurr

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-//require express !
+//require express 
 const  express = require ('express');
+//Instance express
+const app = express();
+// Création Middleware
+app.use(express.json());
+//create Port
+const PORT=8000
+// Lancer le serveur
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`Server is running on PORT ${PORT}`);
+});
