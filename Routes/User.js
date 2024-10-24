e88030befb173c76536c6332b4b292734e234b1b
Malek9707 <cherifmalek48@gmail.com>
2024-10-24 20:04:58 +0100

    deletereset

--- Routes/User.js
+++ Routes/User.js
@@ -1,6 +1,10 @@
-const express=require("express")
-const { register, login } = require("../Controllers/User")
-const router=express.Router()
-router.post("/register",register)
-router.post("/login",login)
-module.exports=router
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.delete('/delete/:_id', userController.deleteUser); // Route pour supprimer un utilisateur
+router.put('/reset-password/:_id', userController.resetPassword); // Route pour réinitialiser le mot de passe
+
+module.exports = router;
