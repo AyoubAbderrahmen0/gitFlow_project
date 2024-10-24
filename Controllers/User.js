c5705179a0fc15b3a7f09b45f68efded7685b90f
Malek9707 <cherifmalek48@gmail.com>
2024-10-24 19:44:02 +0100

    integrationBcrypt

--- Controllers/User.js
+++ Controllers/User.js
@@ -1,15 +1,19 @@
 const User = require('../Models/User');
+const bcrypt = require('bcrypt');
 
 exports.register = async (req, res) => {
   try {
-    const { userName, email, password,age,phone } = req.body;
+    const { userName, email, password, age, phone } = req.body;
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).send({ msg: "Email already exists!" });
     }
 
-    const newUser = new User({ userName, email, password,age,phone });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ userName, email, password: hashedPassword, age, phone });
     await newUser.save();
 
     res.status(200).send({ msg: "User registered successfully!", newUser });
@@ -23,7 +27,12 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (!user || user.password !== password) {
+    if (!user) {
+      return res.status(400).send({ msg: "Email or password invalid!" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
       return res.status(400).send({ msg: "Email or password invalid!" });
     }
 
@@ -32,3 +41,5 @@ exports.login = async (req, res) => {
     res.status(500).send({ msg: "Error on login", error });
   }
 };
+
+
